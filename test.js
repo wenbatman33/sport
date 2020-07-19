87676c7b274d473eb3debef22aa32c77ebcf32f2
batman <batman@flyup.tw>
2020-07-19 13:37:42 +0800

    test

--- test.js
+++ test.js
@@ -1,27 +1,39 @@
+const puppeteer = require('puppeteer');
 const request = require("request");
 const cheerio = require("cheerio");
 const fs = require("fs");
-const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://tixcraft.com/activity/detail/20_ERIC');
-  console.log('object')
   await page.screenshot({path: 'google.png'});
   await page.click('a[target=_new]');
 
   //把網頁的body抓出來
   let body = await page.content()
-  console.log(body)
-
-  //接著我們把他丟給cheerio去處理
-  // let $ = await cheerio.load(body)
-  // const concertDate = "2020/08/09";
-  // const item = $('td:contains("' + concertDate + '")').parent();
-  // const btn = $(item).find($('input[value=立即訂購]'));
-  // console.log(initBtn);
-
+  setTimeout(() => {
+    getPage(body)
+  }, 100);
+  //cheerio去處理
+  
+  function getPage(html){
+    const result = [];
+    let $ = cheerio.load(html)
+    // const concertDate = "2020/08/09";
+    // const item = $('td:contains("' + concertDate + '")').parent();
+    const item = $('.normal')
+    item.map(item=>{
+      console.log(item);
+      const table_td = item.eq(i).find('td');
+      const time = table_td.eq(1).text(); // time (台灣時間)
+      const latitude = table_td.eq(2).text(); // latitude (緯度)
+      const longitude = table_td.eq(3).text(); // longitude (經度)
+      result.push(Object.assign({ time, latitude, longitude }));
+    })
+    // const btn = $(item).html();
+    console.log(result);
+  }
   // await browser.close();
 
 })();
